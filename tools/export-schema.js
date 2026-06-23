c636e2123db94e09e16f55c7a0e1998398e0f92c
Tim Waindi <tunnelandvision@gmail.com>
2026-06-23 12:51:34 -0400

    fix: cast system property to boolean in exported schema json

--- tools/export-schema.js
+++ tools/export-schema.js
@@ -6,6 +6,7 @@ try {
   const collections = db.prepare("SELECT id, name, type, system, fields, indexes, listRule, viewRule, createRule, updateRule, deleteRule, options FROM _collections").all();
 
   collections.forEach(c => {
+    c.system = Boolean(c.system);
     c.fields = JSON.parse(c.fields || '[]');
     c.indexes = JSON.parse(c.indexes || '[]');
     c.options = JSON.parse(c.options || '{}');
